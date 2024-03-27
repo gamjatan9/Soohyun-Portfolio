9b182f331e5999e877622e9143fe3aa702619eae
soohyun <gamjatan9@gmail.com>
2024-03-27 17:31:51 +0900

    🎉 Chore: create next.js app

---
+++ soohyunkim/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import styles from './page.module.scss'
+
+export default function Home() {
+  return (
+    <div>
+      home
+      <h3>안녕하세요</h3>
+      안녕 인녕 안녕
+    </div>
+  )
+}
