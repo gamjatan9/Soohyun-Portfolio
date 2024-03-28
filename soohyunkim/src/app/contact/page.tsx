3d69f95f5c83a4fb52d81dd98ca93afa373c5f13
soohyun <gamjatan9@gmail.com>
2024-03-28 15:32:49 +0900

    ✨ Feat: add pages

---
+++ soohyunkim/src/app/contact/page.tsx
@@ -0,0 +1,9 @@
+export default function Contact() {
+  return (
+    <div>
+      <div>
+        <p>Contact</p>
+      </div>
+    </div>
+  )
+}
--- soohyunkim/src/app/page.tsx
+++ soohyunkim/src/app/page.tsx
@@ -1,12 +1,18 @@
-import Image from 'next/image'
-import styles from './page.module.scss'
+import { MenuBtn } from '../../public/svgs/index'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <div>
-      home
-      <h3>안녕하세요</h3>
-      안녕 인녕 안녕
+      <div className="home-name">
+        <p className="home-title">Soohyun Kim</p>
+        <p className="home-frontend">Frontend</p>
+      </div>
+      <Link href="/menu">
+        <button>
+          <MenuBtn />
+        </button>
+      </Link>
     </div>
   )
 }
