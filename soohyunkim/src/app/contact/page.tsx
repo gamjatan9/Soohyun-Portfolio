2e185afe3335c2d375096e930cd59a47425e401c
soohyun <gamjatan9@gmail.com>
2024-04-03 21:39:49 +0900

    ✨ Feat: contact page 이메일 연결 추가, 디자인 변경

--- soohyunkim/src/app/contact/page.tsx
+++ soohyunkim/src/app/contact/page.tsx
@@ -1,9 +1,13 @@
+import Menu from '@/components/Menu';
+
 export default function Contact() {
   return (
-    <div>
-      <div>
-        <p>Contact</p>
+    <div className="contact-back">
+      <div className="contact-container">
+        <p className="contact-title">Let's work together!</p>
+        <p>이메일 연결하기</p>
+        <a href="mailto:soo000307@egmail.com">soo000307@gmail.com</a>
       </div>
     </div>
-  )
+  );
 }
