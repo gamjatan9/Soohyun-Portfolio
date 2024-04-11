dcf6fb5e0b422d8c7529c0d0d9af9ed6a3daa59c
soohyun <gamjatan9@gmail.com>
2024-04-11 12:09:03 +0900

    📝 Chore: not-found 파일 수정

--- soohyunkim/src/app/not-found.tsx
+++ soohyunkim/src/app/not-found.tsx
@@ -1,13 +1,13 @@
-import Link from 'next/link'
-import { NextPage } from 'next'
+import Link from 'next/link';
+import { NextPage } from 'next';
 
 const NotFound: NextPage = () => {
   return (
-    <div>
+    <div className="not-found">
       <div>이 페이지는 존재하지 않습니다.</div>
       <Link href="/">홈으로 돌아가기</Link>
     </div>
-  )
-}
+  );
+};
 
-export default NotFound
+export default NotFound;
