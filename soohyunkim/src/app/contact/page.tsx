b0b0c01a8a8766c39a0c5a0808dba5a88b7406ad
soohyun <gamjatan9@gmail.com>
2024-04-11 17:15:34 +0900

    🔨 Feat: contact 하단 주소 연결

--- soohyunkim/src/app/contact/page.tsx
+++ soohyunkim/src/app/contact/page.tsx
@@ -1,12 +1,20 @@
-import Menu from '@/components/Menu';
-
 export default function Contact() {
   return (
-    <div className="contact-back">
-      <div className="contact-container">
-        <p className="contact-title">Let's work together!</p>
-        <p>이메일 연결하기</p>
+    <div className="contact">
+      <div className="container">
+        <p className="title">Let's work together!</p>
+        <p className="send-email">이메일 보내기</p>
         <a href="mailto:soo000307@egmail.com">soo000307@gmail.com</a>
+        <div className="more">
+          <div className="item">
+            <span className="text">PHONE</span>
+            <a href="tel:010-0000-0000" className="text-hover">
+              010-0000-0000
+            </a>
+          </div>
+          <div className="item">GITHUB</div>
+          <div className="item">BLOG</div>
+        </div>
       </div>
     </div>
   );
