8029bf829187f82aa7667518e5bc1d34a5f00030
soohyun <gamjatan9@gmail.com>
2024-04-11 11:54:19 +0900

    🍱 Update: svgs index

--- soohyunkim/public/svgs/index.ts
+++ soohyunkim/public/svgs/index.ts
@@ -1,3 +1,7 @@
-import MenuBtn from './menu.svg'
+import MenuBtn from './menu.svg';
+import MenuBtnL from './menu-light.svg';
+import SchoolLog from './schoollog.svg';
+import Portfolio from './portfolio.svg';
+import AllerGenie from './allergenie.svg';
 
-export { MenuBtn }
+export { MenuBtn, MenuBtnL, SchoolLog, Portfolio, AllerGenie };
