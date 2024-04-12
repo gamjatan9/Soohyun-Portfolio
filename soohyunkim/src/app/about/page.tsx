ff6fca4c1a27c3a5f71ec6415348c531182d67ec
soohyun <gamjatan9@gmail.com>
2024-04-12 16:08:40 +0900

    ✨ Feat: about page

--- soohyunkim/src/app/about/page.tsx
+++ soohyunkim/src/app/about/page.tsx
@@ -1,8 +1,23 @@
+import { Profile } from '../../../public/images';
+
 export default function About() {
   return (
     <div className="about">
-      <div className="my-picture">
-        <p>About</p>
+      <div className="border-line"></div>
+      <div className="main-content">
+        <div className="profile-container">
+          <div className="profile"></div>
+        </div>
+        <div className="nav-bar">
+          <div className="nav">
+            <a href="#about">ABOUT</a>
+            <a href="#education">EDUCATION</a>
+            <a href="#experience">EXPERIENCE</a>
+            <a href="#awards">AWARDS</a>
+            <a href="#skills">SKILLS</a>
+          </div>
+        </div>
+        <div className="content"></div>
       </div>
     </div>
   );
